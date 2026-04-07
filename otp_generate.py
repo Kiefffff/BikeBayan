bcfd1c72d14b7822454b2488f6347792b739db1a
Hannah Dungca <hrdungca@up.edu.ph>
2026-04-07 08:32:56 +0800

    Separate OTP auth steps to two files

--- otp_generate.py
+++ otp_generate.py
@@ -4,15 +4,16 @@ from dynaconf import Dynaconf
 config = Dynaconf(settings_files=["./config.toml"], environments=False)
 authenticator = MOSIPAuthenticator(config=config)
 
-# step 2: use OTP and transaction ID in auth request
-# can change function to authenticator.kyc()
-# but don't forget to decrypt the response for that
-response = authenticator.auth(
+# step 1: generate OTP
+response = authenticator.genotp(
     individual_id="5408602380",
     individual_id_type="UIN",
-    otp_value="PLACE OTP HERE",
-    txn_id="PLACE TRANSACTION ID HERE",
-    consent=True,
+    # can pass either one of these
+    email=True,
+    phone=True,
 )
 response_body = response.json()
 print(f"RESPONSE: {response_body}")
+
+# need to get transaction ID from response
+transaction_id = response_body["transactionID"]
